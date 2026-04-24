8700239d309aec7f4f32e7083d62c0f6c84a5289
ikowl <1202407029@students.itspku.ac.id>
2026-04-24 14:19:25 +0700

    belajar php

---
+++ kedua.php
@@ -0,0 +1,10 @@
+<?php
+    $nim = '1202407029';
+    $nama = 'IKO';
+    $alamat = 'Jl. BromoMountain';
+    $nilai = 100;
+    echo "NIM : ", $nim, "<br>";
+    echo "Nama : ", $nama, "<br>";
+    echo "Alamat : ", $alamat, "<br>";
+    echo "Nilai : $nilai";
+?>
